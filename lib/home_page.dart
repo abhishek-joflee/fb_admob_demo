1ba8b06a86a1340f2e4349d7c77cc173d5212c72
Abhishek Patil <abhishek.patil@joflee.com>
2021-10-01 09:35:20 +0530

    banner-ad shown

---
+++ lib/home_page.dart
@@ -0,0 +1,70 @@
+import 'package:facebook_audience_network/facebook_audience_network.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            alignment: const Alignment(0.5, 1),
+            child: FacebookBannerAd(
+              placementId: "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID",
+              bannerSize: BannerSize.STANDARD,
+              listener: (result, value) {
+                switch (result) {
+                  case BannerAdResult.ERROR:
+                    debugPrint("Error: $value");
+                    break;
+                  case BannerAdResult.LOADED:
+                    debugPrint("Loaded: $value");
+                    break;
+                  case BannerAdResult.CLICKED:
+                    debugPrint("Clicked: $value");
+                    break;
+                  case BannerAdResult.LOGGING_IMPRESSION:
+                    debugPrint("Logging Impression: $value");
+                    break;
+                }
+              },
+            ),
+          ),
+          const Text(
+            'You have pushed the button this many times:',
+          ),
+          Text(
+            '$_counter',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
-import 'dart:io';
-
-import 'package:facebook_audience_network/facebook_audience_network.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  FacebookAudienceNetwork.init(
-    testingId: "a2d5521b-8bad-4b85-b597-bc72d9e0e16e",
-    iOSAdvertiserTrackingEnabled: true,
-  );
+import 'home_page.dart';
+
+void main() async {
+  // await FacebookAudienceNetwork.init(
+  //   testingId: "a2d5521b-8bad-4b85-b597-bc72d9e0e16e",
+  // );
   runApp(const MyApp());
 }
 
@@ -22,82 +20,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: [
-          Container(
-            alignment: const Alignment(0.5, 1),
-            child: FacebookBannerAd(
-              placementId: Platform.isAndroid
-                  ? "YOUR_ANDROID_PLACEMENT_ID"
-                  : "YOUR_IOS_PLACEMENT_ID",
-              bannerSize: BannerSize.STANDARD,
-              listener: (result, value) {
-                switch (result) {
-                  case BannerAdResult.ERROR:
-                    debugPrint("Error: $value");
-                    break;
-                  case BannerAdResult.LOADED:
-                    debugPrint("Loaded: $value");
-                    break;
-                  case BannerAdResult.CLICKED:
-                    debugPrint("Clicked: $value");
-                    break;
-                  case BannerAdResult.LOGGING_IMPRESSION:
-                    debugPrint("Logging Impression: $value");
-                    break;
-                }
-              },
-            ),
-          ),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'You have pushed the button this many times:',
-                ),
-                Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
 }
